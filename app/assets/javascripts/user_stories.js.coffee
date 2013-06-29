422365db2620432ad5970978e14358ea5b319b2b
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-06-29 14:49:27 -0400

    Sortable user stories

---
+++ app/assets/javascripts/user_stories.js.coffee
@@ -0,0 +1,2 @@
+jQuery ->
+  $('#user_stories').sortable()
--- app/controllers/user_stories_controller.rb
+++ app/controllers/user_stories_controller.rb
@@ -2,6 +2,11 @@ class UserStoriesController < ApplicationController
   def new
     @project = Project.find(params[:project_id])
     @user_story = UserStory.new
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def create
@@ -19,18 +24,21 @@ class UserStoriesController < ApplicationController
   def edit
     @project = Project.find(params[:project_id])
     @user_story = @project.user_stories.find(params[:id])
+
+    render "edit.js"
   end
 
   def update
     @project = Project.find(params[:project_id])
     @user_story = @project.user_stories.find(params[:id])
+    @user_stories = UserStoryDecorator.decorate_collection(@project.user_stories.order("created_at"))
+    @users = UserDecorator.decorate_collection(@project.users)
+    @team = @project.team
 
-    if @user_story.update_attributes(params[:user_story])
-      flash[:notice] = "Story updated!"
-    else
-      flash[:notice] = "There was an error updating your story"
+    @user_story.update_attributes(params[:user_story])
+    respond_to do |format|
+      format.js
     end
-    redirect_to @project
   end
 
   def destroy
