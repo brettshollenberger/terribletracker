6b64b0d46b6be670276bacdc613faa641209ca4b
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-07-01 21:47:33 -0400

    Add spec for comment decorator

--- app/decorators/comment_decorator.rb
+++ app/decorators/comment_decorator.rb
@@ -1,8 +1,9 @@
 class CommentDecorator < Draper::Decorator
   delegate_all
 
-  def pretty_created_at
-    a = (Time.now-self.created_at).to_i
+  # Clock argument for testing; defaults to Time
+  def pretty_created_at(clock=Time.now)
+    a = (clock-self.created_at).to_i
 
     return 'just now' if a == 0
     return 'a second ago' if a == 1
@@ -12,19 +13,19 @@ class CommentDecorator < Draper::Decorator
     return 'an hour ago' if a >= 3541 && a <= 7100 # 3600 = 1 hour
     return "Today at#{self.created_at.strftime("%l:%M%p")}" if a >= 7101 && a <= 82800
     return "Yesterday at#{self.created_at.strftime("%l:%M%p")}" if a >= 82801 && a <= 165600
-    if Time.now.strftime("%A") == "Sunday"
-      return "This #{self.created_at.strftime("%A")} at#{self.created_at.strftime("%l:%M%p")}" if a >= 165601 && a <= 579600
-      return "#{self.created_at.strftime("%A %B %e")} at#{self.created_at.strftime("%l:%M%p")}" if a >= 579601
-    elsif Time.now.strftime("%A") == "Saturday"
+    if clock.strftime("%A") == "Sunday"
+      return "This #{self.created_at.strftime("%A")} at #{self.created_at.strftime("%l:%M%p")}" if a >= 165601 && a <= 579600
+      return "#{self.created_at.strftime("%A %B %e")} at #{self.created_at.strftime("%l:%M%p")}" if a >= 579601
+    elsif clock.strftime("%A") == "Saturday"
       return "This #{self.created_at.strftime("%A")} at#{self.created_at.strftime("%l:%M%p")}" if a >= 165601 && a <= 496800
       return "#{self.created_at.strftime("%A %B %e")} at#{self.created_at.strftime("%l:%M%p")}" if a >= 496801
-    elsif Time.now.strftime("%A") == "Friday"
+    elsif clock.strftime("%A") == "Friday"
       return "This #{self.created_at.strftime("%A")} at#{self.created_at.strftime("%l:%M%p")}" if a >= 165601 && a <= 414000
       return "#{self.created_at.strftime("%A %B %e")} at#{self.created_at.strftime("%l:%M%p")}" if a >= 414001
-    elsif Time.now.strftime("%A") == "Thursday"
+    elsif clock.strftime("%A") == "Thursday"
       return "This #{self.created_at.strftime("%A")} at#{self.created_at.strftime("%l:%M%p")}" if a >= 165601 && a <= 331200
       return "#{self.created_at.strftime("%A %B %e")} at#{self.created_at.strftime("%l:%M%p")}" if a >= 331201
-    elsif Time.now.strftime("%A") == "Wednesday"
+    elsif clock.strftime("%A") == "Wednesday"
       return "This #{self.created_at.strftime("%A")} at#{self.created_at.strftime("%l:%M%p")}" if a >= 165601 && a <= 248400
       return "#{self.created_at.strftime("%A %B %e")} at#{self.created_at.strftime("%l:%M%p")}" if a >= 248401
     else
