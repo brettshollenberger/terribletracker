242baf9edb08598c40ea1f73b2cb0eb3e83074e1
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-07-02 20:54:43 -0400

    Add specs for team activity page

---
+++ spec/features/team_page_displays_recent_activity_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+feature "team page displays the team's recent activity", %q{
+  As a user,
+  I want to see the recent activity of my teams,
+  so that I can know what's happened while I was away.
+} do
+  # Acceptance Criteria:
+  # User visits the homepage, and sees the recent activity
+  # of their teams.
+
+  context 'as a user' do
+
+    background do
+      create_team_with_project
+      login(@owner)
+      visit_team_path(@team)
+    end
+
+    scenario "seeing team's activity", js: true do
+      find('#body-main').should have_content("Recent Activity")
+      find('#body-main').should have_content("#{@owner.full_name} created user story #{@story.title}")
+    end
+  end
+end
