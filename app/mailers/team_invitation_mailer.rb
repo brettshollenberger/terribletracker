f8c74ec22250ffa490e8c09f6d71b68a0ebb8f55
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-07-03 21:40:45 -0400

    Fix user invite emails for heroku

--- app/mailers/team_invitation_mailer.rb
+++ app/mailers/team_invitation_mailer.rb
@@ -8,7 +8,7 @@ class TeamInvitationMailer < ActionMailer::Base
     @membership = Membership.where(user_id: @user, joinable_id: @team.id, joinable_type: "Team").first
     @inviter = @membership.inviter
     @url = "#{@base_url}/teams/#{@team.id}"
-    @accept_url = "http://0.0.0.0:3000/membership/#{@membership.id}/accept_team"
+    @accept_url = "#{@base_url}/membership/#{@membership.id}/accept_team"
     mail(:to => "#{@user.email}", :subject => "You've been invited to #{@team.name} on Terrible Tracker")
   end
 end
