a90cda11cac618acc2596d310240292f4cdf1d1b
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-07-05 19:12:07 -0400

    Cleaning up js partials

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @activities = current_user.recent_activities.limit(5)
+    @activities = current_user.recent_activities
     respond_to do |format|
       format.html
       format.js
